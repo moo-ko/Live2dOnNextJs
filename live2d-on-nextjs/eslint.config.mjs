83b24cd6be9262fac66c70eb55e251346baf6bd0
D:\home\YaMaNeX\Apps\SourceTree <yamanex@msn.com>
2025-02-25 16:23:35 +0000

    eslint 9 対応

---
+++ live2d-on-nextjs/eslint.config.mjs
@@ -0,0 +1,22 @@
+import { FlatCompat } from '@eslint/eslintrc';
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript'],
+  }),
+  {
+    files: ['src/**/*.ts', 'src/**/*.tsx'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      'react-hooks/exhaustive-deps': 'off',
+    },
+  },
+];
+
+export default eslintConfig;
